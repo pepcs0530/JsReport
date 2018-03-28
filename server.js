51944aa5572c481f834eec1b34f96dbdd365a55f
APIWICH SUNGMUANG (CDGS-ADD) <apiwich.s@cdg.co.th>
2018-03-28 11:02:49 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var jsreport = require('jsreport')({ 
+	"scripts": {
+	"allowedModules" : ["http"]
+	} })
+
+if (process.env.JSREPORT_CLI) {
+  // export jsreport instance to make it possible to use jsreport-cli
+  module.exports = jsreport
+} else {
+  jsreport.init().then(function () {
+    // running
+  }).catch(function (e) {
+    // error during startup
+    console.error(e.stack)
+    process.exit(1)
+  })
+}
